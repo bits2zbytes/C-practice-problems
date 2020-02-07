b42cd4a1053f9ffc6f8cc3cce3a87bdf990d4d3d
Monika Jha <55924540+bits2zbytes@users.noreply.github.com>
2020-02-07 09:15:57 +0530

    Add files via upload

---
+++ arrayRotation.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+//rotates array of size n by d elements
+//takes auxillary space = d
+void arrRotation(int arr[],int n,int d){
+    int tempArr[10000];
+
+    for(int i=0;i<d;i++){
+        tempArr[i]=arr[i];
+    }
+
+    for(int i=0;i<n-d;i++){
+        arr[i] = arr[i+d];
+    }
+
+    for(int i=n-d,j=0;i<n;i++,j++){
+        arr[i] = tempArr[j];
+    }
+
+return;
+
+}
+
+//rotates array of size n by d elements
+//using one temp variable
+void arrRotation1(int arr[],int n,int d){
+    int temp;
+
+    for(int i=0;i<d;i++){
+        temp = arr[0];
+
+        for(int i=0;i<n-1;i++){
+            arr[i] = arr[i+1];
+        }
+
+        arr[n-1] = temp;
+    }
+
+return;
+
+}
+
+int main(){
+    int arr[1000];
+    int n,d;
+
+    cin>>n>>d;
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    arrRotation1(arr,n,d);
+
+     for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+return 0;
+}
